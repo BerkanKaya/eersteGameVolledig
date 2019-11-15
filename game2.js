f8ca583b6fd325e6ac73ffe4e029174e85b70e31
BerkanKaya <99059255@mydavinci.nl>
2019-11-15 11:19:29 +0100

    met geneste ifs

--- game2.js
+++ game2.js
@@ -4,158 +4,183 @@ var keuzes = prompt ("De eerste keuze is om de amerikaanse president te vermoord
 	if(keuzes == "1"){
 			alert("Oke je hebt dus er voor gekozen om de amerikaanse president te vermoorden");
 			var plan = prompt("De bieder vraagt of je een sniper of bazooka wilt om Trump te vermoorden");
-			}
+			
 
-	if(plan == "sniper"){
-			alert("Oke je hebt dus voor de sniper gekozen, en wilt hem in stilte vermoorden");
-			alert("De bieder verteld je dat Trump een afspraak heeft bij de macdonalds, zaterdag om 2uur");
-			alert("Je zet je wapen klaar op de gegeven locatie");
-			alert("3 uur later....");
-			alert("Je ziet trump bij de MacDonalds, en je staat klaar met de sniper in je hand");
-			alert("Je richt op Trump, maar er staan bewakers naast hem...");
-			alert("Er is een kans dat je Trump mist, en een bewaker raakt");
-			var plan2 = prompt("Schiet je meteen?");
-			}
+		if(plan == "sniper"){
+				alert("Oke je hebt dus voor de sniper gekozen, en wilt hem in stilte vermoorden");
+				alert("De bieder verteld je dat Trump een afspraak heeft bij de macdonalds, zaterdag om 2uur");
+				alert("Je zet je wapen klaar op de gegeven locatie");
+				alert("3 uur later....");
+				alert("Je ziet trump bij de MacDonalds, en je staat klaar met de sniper in je hand");
+				alert("Je richt op Trump, maar er staan bewakers naast hem...");
+				alert("Er is een kans dat je Trump mist, en een bewaker raakt");
+				var plan2 = prompt("Schiet je meteen?");
+				
 
-	if(plan2 == "ja"){
-			alert("JE SCHIET....");
-			alert("Je RAAKT Trump maar ook een kind met wie hij aan het praten was...");
-			var plan3 = prompt("Ga je naar de kind zijn moeder toe om sorry te zeggen?");
-			}
+			if(plan2 == "ja"){
+					alert("JE SCHIET....");
+					alert("Je RAAKT Trump maar ook een kind met wie hij aan het praten was...");
+					var plan3 = prompt("Ga je naar de kind zijn moeder toe om sorry te zeggen?");
 
-	if(plan3 == "ja"){
-			alert("Je loop naar de moeder toe, en zegt sorry");
-			alert("ZE WORD GEK EN BELT DE POLITIE");
-			var plan4 = prompt("Vermoord je haar om te voorkomen dat ze de politie belt?");
-			}
+					if(plan2 == "nee"){
+						alert("Je wacht tot Trump klaar is met eten...");
+						alert("Maar een bewaker spot je en waarschuwed iedereen");			
+						alert("Jij merkt niet dat ze je zien en er staan opeens 10 politie auto's om je heen");
+						alert("U lose");
+					    
+					
+			
+					if(plan3 == "ja"){
+						alert("Je loop naar de moeder toe, en zegt sorry");
+						alert("ZE WORD GEK EN BELT DE POLITIE");
+						var plan4 = prompt("Vermoord je haar om te voorkomen dat ze de politie belt?");
+							if(plan4 == "ja"){
+								alert("Je steekt haar, en rent weg");
+								alert("Je gaat naar de bieder, en pakt de 5miljoen euro");
+								alert("Je verhuist naar het buitenland, en word nooit gevonden");
+								alert("U wint");
 
-	if(plan4 == "ja"){
-			alert("Je steekt haar, en rent weg");
-			alert("Je gaat naar de bieder, en pakt de 5miljoen euro");
-			alert("Je verhuist naar het buitenland, en word nooit gevonden");
-			alert("U wint");
-			}
+								 if(plan3 == "nee"){
+									alert("Het is niet zo netjes om geen sorry te zeggen...");
+									alert("U LOSE");
+									}
+								}
+							}
 	
+			}
+				}
+			}
+			
+			
+
+			
+			else if (plan == "bazooka"){
+				alert("Oke je hebt dus voor de bazooka gekozen");
+				alert("De bieder zegt dat dat de domste keuze ooit is, en zegt dat je de baan niet krijgt")
+				var bazooka1 = prompt("Wil je de bieder vermoorden uit woede?");
+
+				if(bazooka1 == "ja"){
+					alert("Je steekt de bieder stiekem neer");
+					alert("Hij heeft een koffer bij zich, je pakt het op");
+					alert("Je rijd naar huis..");
+					alert("Je kijkt in de koffer en zit 10 miljoen euro in de koffer!");
+					alert("Je hoort een maffia bende voor je deur, en ze trappe naar binnen")
+					alert("Ze vermoorden je en pakken de koffer");
+
+					if(bazooka1 == "nee"){
+						alert("De man loopt weg, en jij hebt geen job meer");
+						alert("Goedzo");
+						}
+					}
+					}
+					
+				
+
+	}
 	else if (keuzes == "2"){
 			alert("Oke je hebt er dus voor gekozen om je beste vriends moeder te vermoorden, focktop");
 			var keuzes1 = prompt("Wil je haar stiekem vermoorden of openbaar?")
-			}
+			
 
-	if(keuzes1 == "stiekem"){
-			alert("Oke je wilt haar dus stiekem vermoorden");
-			var keuzes2 = prompt("Met een sniper of met een revolver?");
-			}
+		if(keuzes1 == "stiekem"){
+				alert("Oke je wilt haar dus stiekem vermoorden");
+				var keuzes2 = prompt("Met een sniper of met een revolver?");
+				
 
- 	if(keuzes2 == "sniper"){
-			alert("Je kiest voor de sniper");
-			alert("Je zoekt een goed plekje op in de weekend om haar te snipen");
-			var keuzes3 = prompt("Bij de supermarkt of voor haar deur?");
-			}
+		 	if(keuzes2 == "sniper"){
+					alert("Je kiest voor de sniper");
+					alert("Je zoekt een goed plekje op in de weekend om haar te snipen");
+					var keuzes3 = prompt("Bij de supermarkt of voor haar deur?");
+			
 
-	if(keuzes3 == "supermarkt"){
-			alert("Je pakt de sniper");
-			alert("Je ziet haar");
-			alert("Je staat klaar om te schieten");
-			var keuzes4 = prompt("normaal schieten of trickshotten voor een clip?");
-			}
+				if(keuzes3 == "supermarkt"){
+						alert("Je pakt de sniper");
+						alert("Je ziet haar");
+						alert("Je staat klaar om te schieten");
+						var keuzes4 = prompt("normaal schieten of trickshotten voor een clip?");
+						
 
-	if(keuzes4 == "normaal"){
-			alert("Je schiet");
-			alert("Ze is dood");
-			alert("Je ontsnapt, pakt de miljoen euro, en ben nooit meer te zien");
-			}
+					if(keuzes4 == "normaal"){
+							alert("Je schiet");
+							alert("Ze is dood");
+							alert("Je ontsnapt, pakt de miljoen euro, en ben nooit meer te zien");
 
-	if(keuzes1 == "openbaar"){
-			alert("Oke je wilt haar dus ook nog openbaar vermoorden");
-			var keuzes2 = prompt("Met een stok of met een auto?");
-			}
+							if(keuzes4 == "trickshotten"){
+								alert("JE SPRINGT VAN HET DAK AF");
+								alert("JE DOET RARE BEWEGINGEN");
+								alert("JE DRAAIT ROND");
+								alert("JE SCHIET ZONDER TE RICHTEN");
+								alert("JE MIST");
+								alert("en je bent dood...");
+								alert("probeer dat nooit meer...");
 
- 	if(keuzes2 == "stok"){
-			alert("Je pakt de stok");
-			alert("Je loop naar haar toe...");
-			alert("Je probeert haar dood te slaan, maar iemand helpt haar, en slaat jou dood");
-			alert("U died");
-			}
+								if(keuzes3 == "deur"){
+									alert("Je pakt de sniper");
+									alert("Je ziet haar");
+									alert("Je staat klaar om te schieten");
+									var keuzes6 = prompt("normaal schieten of trickshotten voor een clip?");
 
-	else if (plan == "bazooka"){
-			alert("Oke je hebt dus voor de bazooka gekozen");
-			alert("De bieder zegt dat dat de domste keuze ooit is, en zegt dat je de baan niet krijgt")
-			var bazooka1 = prompt("Wil je de bieder vermoorden uit woede?");
-			}
+									if(keuzes6 == "trickshotten"){
+										alert("JE SPRINGT VAN HET DAK AF");
+										alert("JE DOET RARE BEWEGINGEN");
+										alert("JE DRAAIT ROND");
+										alert("JE SCHIET ZONDER TE RICHTEN");
+										alert("JE MIST");
+										alert("en je bent dood...");
+										alert("probeer dat nooit meer...");
 
-	if(bazooka1 == "ja"){
-			alert("Je steekt de bieder stiekem neer");
-			alert("Hij heeft een koffer bij zich, je pakt het op");
-			alert("Je rijd naar huis..");
-			alert("Je kijkt in de koffer en zit 10 miljoen euro in de koffer!");
-			alert("Je hoort een maffia bende voor je deur, en ze trappe naar binnen")
-			alert("Ze vermoorden je en pakken de koffer");
+										if(keuzes6 == "normaal"){
+											alert("Je schiet");
+											alert("Ze is dood");
+											alert("Je ontsnapt, pakt de miljoen euro, en ben nooit meer te zien");
 			}
-
-	if(keuzes2 == "revolver"){
-			alert("Je kiest voor de revolver");
-			alert("Je zoekt een goed plekje op om haar dood te schieten");
-			alert("Je ziet haar recht voor je staan");
-			alert("Je schiet");
-			alert("Je mist en schiet een kind dood");
-			alert("GAME OVER");
 			}
-
-	if(keuzes4 == "trickshotten"){
-			alert("JE SPRINGT VAN HET DAK AF");
-			alert("JE DOET RARE BEWEGINGEN");
-			alert("JE DRAAIT ROND");
-			alert("JE SCHIET ZONDER TE RICHTEN");
-			alert("JE MIST");
-			alert("en je bent dood...");
-			alert("probeer dat nooit meer...");
 			}
-
-	if(keuzes3 == "deur"){
-			alert("Je pakt de sniper");
-			alert("Je ziet haar");
-			alert("Je staat klaar om te schieten");
-			var keuzes6 = prompt("normaal schieten of trickshotten voor een clip?");
+				}
 			}
+							}
+						}
+					}
 
-	if(keuzes6 == "trickshotten"){
-			alert("JE SPRINGT VAN HET DAK AF");
-			alert("JE DOET RARE BEWEGINGEN");
-			alert("JE DRAAIT ROND");
-			alert("JE SCHIET ZONDER TE RICHTEN");
-			alert("JE MIST");
-			alert("en je bent dood...");
-			alert("probeer dat nooit meer...");
+					if(keuzes2 == "revolver"){
+						alert("Je kiest voor de revolver");
+						alert("Je zoekt een goed plekje op om haar dood te schieten");
+						alert("Je ziet haar recht voor je staan");
+						alert("Je schiet");
+						alert("Je mist en schiet een kind dood");
+						alert("GAME OVER");
 			}
+						
 
-	if(keuzes6 == "normaal"){
-			alert("Je schiet");
-			alert("Ze is dood");
-			alert("Je ontsnapt, pakt de miljoen euro, en ben nooit meer te zien");
+
+		if(keuzes1 == "openbaar"){
+				alert("Oke je wilt haar dus ook nog openbaar vermoorden");
+				var keuzes2 = prompt("Met een stok of met een auto?");
+				
+
+		 	if(keuzes2 == "stok"){
+					alert("Je pakt de stok");
+					alert("Je loop naar haar toe...");
+					alert("Je probeert haar dood te slaan, maar iemand helpt haar, en slaat jou dood");
+					alert("U died");
+
+
+				if(keuzes2 == "auto"){
+						alert("Je pakt een rode ferarri, zodat de bloed camoufleerd");
+						alert("Je ziet haar");
+						alert("Je gaat in sportstand, 700pk, 345 km/u op haar af");
+						alert("Je raakt haar, en ze is dood");
+						alert("Maar je rijd zelf de muur in en gaat ook dood");
+						alert("End");
 			}
+					} }
+				}
+			
 
-	if(keuzes2 == "auto"){
-			alert("Je pakt een rode ferarri, zodat de bloed camoufleerd");
-			alert("Je ziet haar");
-			alert("Je gaat in sportstand, 700pk, 345 km/u op haar af");
-			alert("Je raakt haar, en ze is dood");
-			alert("Maar je rijd zelf de muur in en gaat ook dood");
-			alert("End");
-			}
 
-	if(bazooka1 == "nee"){
-			alert("De man loopt weg, en jij hebt geen job meer");
-			alert("Goedzo");
-			}
 
-	if(plan2 == "nee"){
-			alert("Je wacht tot Trump klaar is met eten...");
-			alert("Maar een bewaker spot je en waarschuwed iedereen");			
-			alert("Jij merkt niet dat ze je zien en er staan opeens 10 politie auto's om je heen");
-			alert("U lose");
-		    }
 
-	 if(plan3 == "nee"){
-			alert("Het is niet zo netjes om geen sorry te zeggen...");
-			alert("U LOSE");
-			}
+
+	
+
+	
